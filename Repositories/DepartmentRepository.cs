f4026d7d2b836f0bdd2195a3de4ee12b120c813b
Bakhodir Khaytimbetov <bahodir_haytimbetov@icloud.com>
2026-01-12 17:08:22 +0500

    Department/Role Repo updated

--- Repositories/DepartmentRepository.cs
+++ Repositories/DepartmentRepository.cs
@@ -1,11 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Connect_EFCore.Data;
+using Connect_EFCore.Entities;
 
 namespace Connect_EFCore.Repositories
 {
     public class DepartmentRepository
     {
+        private readonly AppDbContext _context = new AppDbContext();
+
+        public List<Department> GetAll()
+        {
+            return _context.Departments.ToList();
+        }
 
     }
 }
--- Repositories/RoleRepository.cs
+++ Repositories/RoleRepository.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Connect_EFCore.Data;
+using Connect_EFCore.Entities;
 
 namespace Connect_EFCore.Repositories
 {
     public class RoleRepository
     {
+        private readonly AppDbContext _context = new AppDbContext();
+
+
+        public List<Role> GetAll()
+        {
+            return _context.Roles.ToList();
+        }
+
     }
 }
